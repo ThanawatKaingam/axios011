1dfa34a8ade34b670c8d60659342716463c3f1de
ThanawatKaingam <68525371+ThanawatKaingam@users.noreply.github.com>
2020-10-31 00:29:38 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <itune/>
+  </div>
+</template>
+
+<script>
+import itune from './components/itune.vue'
+
+export default {
+  name: 'App',
+  components: {
+    itune
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
